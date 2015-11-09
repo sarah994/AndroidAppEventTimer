01d222af94b6bd1742688b6a53134ef22d333fb5
sarah994 <hansensarah1@hotmail.com>
2015-11-09 11:56:13 +0100

    nieuwe klasses toegvoegd. starten aan eventTimerActivity. layouts, fileIO, RSS allemaal code --> testen

--- app/src/main/java/com/example/r0462870/eventtimer/FileIO.java
+++ app/src/main/java/com/example/r0462870/eventtimer/FileIO.java
@@ -1,7 +1,91 @@
 package com.example.r0462870.eventtimer;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 /**
  * Created by r0462870 on 9/11/2015.
  */
 public class FileIO {
+    private final String URL_STRING = "https://github.com/sarah994/TimerDataXml/blob/master/boss_timers.xml";
+    private final String FILENAME = "boss_timers.xml";
+    private Context context = null;
+
+    public FileIO (Context context) {
+        this.context = context;
+    }
+
+    public void downloadFile() {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+
+            try {
+                // get the URL
+                URL url = new URL(URL_STRING);
+
+                // get the input stream
+                InputStream in = url.openStream();
+
+                // get the output stream
+                FileOutputStream out =
+                        context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+
+                // read input and write output
+                byte[] buffer = new byte[1024];
+                int bytesRead = in.read(buffer);
+                while (bytesRead != -1) {
+                    out.write(buffer, 0, bytesRead);
+                    bytesRead = in.read(buffer);
+                }
+                out.close();
+                in.close();
+            } catch (IOException e) {
+                Log.e("News reader", e.toString());
+            }
+        }
+    }
+
+    public RSSFeed readFile() {
+        try {
+            // get the XML reader
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            XMLReader xmlreader = parser.getXMLReader();
+
+            // set content handler
+            RSSFeedHandler theRssHandler = new RSSFeedHandler();
+            xmlreader.setContentHandler(theRssHandler);
+
+            // read the file from internal storage
+            FileInputStream in = context.openFileInput(FILENAME);
+
+            // parse the data
+            InputSource is = new InputSource(in);
+            xmlreader.parse(is);
+
+            // set the feed in the activity
+            RSSFeed feed = theRssHandler.getFeed();
+            return feed;
+        }
+        catch (Exception e) {
+            Log.e("News reader", e.toString());
+            return null;
+        }
+    }
 }
